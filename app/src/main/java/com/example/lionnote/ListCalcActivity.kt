e21ad69077602a120e89ba0683250206a031d7f8
Eduardo <eduardo.hbds@gmail.com>
2023-06-28 11:09:47 -0300

    feat: add get register by type

--- app/src/main/java/com/example/lionnote/ListCalcActivity.kt
+++ app/src/main/java/com/example/lionnote/ListCalcActivity.kt
@@ -8,6 +8,14 @@ class ListCalcActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_calc)
         //se não chegar nada joga um erro
-        val type = intent?.extras?.getString("type") ?: throw IllegalStateException("Type not found")
+        val type =
+            intent?.extras?.getString("type") ?: throw IllegalStateException("Type not found")
+        Thread {
+            val app = application as App
+            val dao = app.db.calcDao()
+            val response = dao.getRegisterByType(type)
+            runOnUiThread {//usado para escrever na thread principal
+            }
+        }.start()
     }
 }
